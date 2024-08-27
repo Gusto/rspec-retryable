251e13c890420f1e2b79abdd16e406b9597152ad
Ngan Pham <ngan@users.noreply.github.com>
2024-08-27 11:44:43 -0700

    Initial commit

---
+++ lib/rspec/retryable.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "retryable/version"
+
+module Rspec
+  module Retryable
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rspec/retryable_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rspec::Retryable do
+  it "has a version number" do
+    expect(Rspec::Retryable::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
